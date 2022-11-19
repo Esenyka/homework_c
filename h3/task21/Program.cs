160446ca6aeca384e59483320c6cc714526de352
eJwtzE2PwiAUheH/wroLaCVt2fqRzGRiYsbJLAnQq0OLXIRWR43/XWK6OznJ8z6IMgZS2uMAngjSVhraqqYtVbXm2tS0YcArxjkwMIsDb0ooDWWkICHixXYQZ/qYS3KcU8c/lBu/6rE/19vzT7P6aod135f7z+Vmd/+4XwI1v9/M59SbyPEWIDsNKkLMbzL4PlR3sl6ESTtr5AC3IoLqBMZjHgGLKUEUcFLWFVeMw8HhNWP4DzZCkmokwk/OPZ8vJpVP6w== <esenia1317@gmail.com>
2022-11-19 12:01:27 +0700

    added task21

---
+++ h3/task21/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Input A(x)");
+double x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input A(y)");
+double y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input A(z)");
+double z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input B(x)");
+double x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input B(y)");
+double y2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input B(z)");
+double z2 = Convert.ToInt32(Console.ReadLine());
+
+double x = x1 - x2;
+double y = y1 - y2;
+double z = z1 - z2;
+
+double answer = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2) + Math.Pow(z, 2));
+
+Console.WriteLine(Math.Round(answer, 2));
