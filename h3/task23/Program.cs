3dc63e8034cc47695c869fd6e3c0c3b0f3e93831
eJwtzE2PwiAUheH/wroLaCVt2fqRzGRiYsbJLAnQq0OLXIRWR43/XWK6OznJ8z6IMgZS2uMAngjSVhraqqYtVbXm2tS0YcArxjkwMIsDb0ooDWWkICHixXYQZ/qYS3KcU8c/lBu/6rE/19vzT7P6aod135f7z+Vmd/+4XwI1v9/M59SbyPEWIDsNKkLMbzL4PlR3sl6ESTtr5AC3IoLqBMZjHgGLKUEUcFLWFVeMw8HhNWP4DzZCkmokwk/OPZ8vJpVP6w== <esenia1317@gmail.com>
2022-11-19 12:05:44 +0700

    added task 23

---
+++ h3/task23/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Input number - ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+while (count != number)
+{
+    count++;
+    Console.WriteLine(count * count * count);
+}
