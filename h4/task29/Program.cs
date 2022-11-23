68e83da226919463394b100d243fd3d7a1a4800c
eJwtzE2PwiAUheH/wroLaCVt2fqRzGRiYsbJLAnQq0OLXIRWR43/XWK6OznJ8z6IMgZS2uMAngjSVhraqqYtVbXm2tS0YcArxjkwMIsDb0ooDWWkICHixXYQZ/qYS3KcU8c/lBu/6rE/19vzT7P6aod135f7z+Vmd/+4XwI1v9/M59SbyPEWIDsNKkLMbzL4PlR3sl6ESTtr5AC3IoLqBMZjHgGLKUEUcFLWFVeMw8HhNWP4DzZCkmokwk/OPZ8vJpVP6w== <esenia1317@gmail.com>
2022-11-23 13:17:33 +0700

    added task 29

---
+++ h4/task29/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите длину массива - ");
+int len = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = new int[len];
+int count = 0;
+
+while (count != len)
+{
+    int number = new Random().Next(1, 100);
+    arr[count] = number;
+    count++;
+}
+Console.Write("[" + string.Join(", ", arr) + "]");
