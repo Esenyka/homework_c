8c13e0f805f68fea7d880c5cf701cfb73b6a0b89
eJwtzE2PwiAUheH/wroLaCVt2fqRzGRiYsbJLAnQq0OLXIRWR43/XWK6OznJ8z6IMgZS2uMAngjSVhraqqYtVbXm2tS0YcArxjkwMIsDb0ooDWWkICHixXYQZ/qYS3KcU8c/lBu/6rE/19vzT7P6aod135f7z+Vmd/+4XwI1v9/M59SbyPEWIDsNKkLMbzL4PlR3sl6ESTtr5AC3IoLqBMZjHgGLKUEUcFLWFVeMw8HhNWP4DzZCkmokwk/OPZ8vJpVP6w== <esenia1317@gmail.com>
2022-11-26 15:13:35 +0700

    added task36

---
+++ h5/task36/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] FillArray(int size)
+{
+    int[] ar = new int[size];
+    for (int i=0; i<size; i++)
+    {
+        ar[i] = new Random().Next(0, 10);
+    }
+    return ar;
+}
+
+int SumOdd(int[] coll, int size)
+{
+    int count = 0;
+    for(int i=0; i<size; i++)
+    {
+        if (i % 2 != 0)
+        {
+            count = count + coll[i];
+        }
+    }
+    return count;
+}
+
+Console.WriteLine("Введите длину массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] collection = FillArray(size);
+Console.WriteLine("[" + string.Join(", ", collection) + "]");
+
+Console.WriteLine($"Сумма нечетных чисел = {SumOdd(collection, size)}");
