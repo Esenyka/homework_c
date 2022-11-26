daec0996dc3f48d7eae9753041106e2737688ae9
eJwtzE2PwiAUheH/wroLaCVt2fqRzGRiYsbJLAnQq0OLXIRWR43/XWK6OznJ8z6IMgZS2uMAngjSVhraqqYtVbXm2tS0YcArxjkwMIsDb0ooDWWkICHixXYQZ/qYS3KcU8c/lBu/6rE/19vzT7P6aod135f7z+Vmd/+4XwI1v9/M59SbyPEWIDsNKkLMbzL4PlR3sl6ESTtr5AC3IoLqBMZjHgGLKUEUcFLWFVeMw8HhNWP4DzZCkmokwk/OPZ8vJpVP6w== <esenia1317@gmail.com>
2022-11-26 15:01:14 +0700

    added task35

---
+++ h5/task35/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] FillArray(int size)
+{
+    int[] ar = new int[size];
+    for (int i=0; i<size; i++)
+    {
+        ar[i] = new Random().Next(0, 500);
+    }
+    return ar;
+}
+
+void ElBetween(int[] arr, int mi, int ma)
+{
+   int count = 0;
+   for(int i=0; i<arr.Length; i++)
+   {
+       if (arr[i] > mi && arr[i] < ma)
+       {
+           count++;
+       }
+   }
+   Console.WriteLine($"{count} числел в промежутке с {mi} до {ma}");
+}
+
+Console.WriteLine("Введите длину массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите минимальное число: ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите максимальное число: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] array = FillArray(size);
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+ElBetween(array, min, max);
