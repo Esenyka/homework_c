94f3af1e81a4a20b76a40e3e48e251c59dfbd52f
eJwtzE2PwiAUheH/wroLaCVt2fqRzGRiYsbJLAnQq0OLXIRWR43/XWK6OznJ8z6IMgZS2uMAngjSVhraqqYtVbXm2tS0YcArxjkwMIsDb0ooDWWkICHixXYQZ/qYS3KcU8c/lBu/6rE/19vzT7P6aod135f7z+Vmd/+4XwI1v9/M59SbyPEWIDsNKkLMbzL4PlR3sl6ESTtr5AC3IoLqBMZjHgGLKUEUcFLWFVeMw8HhNWP4DzZCkmokwk/OPZ8vJpVP6w== <esenia1317@gmail.com>
2022-11-26 14:56:15 +0700

    added task 34

---
+++ h5/task34/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] FillArray(int size)
+{
+    int[] array = new int[size];
+    for (int i=0; i<size; i++)
+    {
+        array[i] = new Random().Next(100, 999);
+    }
+    return array;
+}
+
+int evenNum(int[] coll, int size)
+{
+    int count = 0;
+    for(int i=0; i<size; i++)
+    {
+        if (coll[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.Write("Введите длину массива: ");
+int userSize = Convert.ToInt32(Console.ReadLine());
+
+int[] numbers = FillArray(userSize);
+Console.WriteLine("[" + string.Join( ", ", numbers) + "]");
+
+Console.WriteLine($"Колличество четных чисел = {evenNum(numbers, userSize)}");
