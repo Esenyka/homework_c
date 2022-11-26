38055de3efe5b38cf8c0924fd41794f8bbfa574c
eJwtzE2PwiAUheH/wroLaCVt2fqRzGRiYsbJLAnQq0OLXIRWR43/XWK6OznJ8z6IMgZS2uMAngjSVhraqqYtVbXm2tS0YcArxjkwMIsDb0ooDWWkICHixXYQZ/qYS3KcU8c/lBu/6rE/19vzT7P6aod135f7z+Vmd/+4XwI1v9/M59SbyPEWIDsNKkLMbzL4PlR3sl6ESTtr5AC3IoLqBMZjHgGLKUEUcFLWFVeMw8HhNWP4DzZCkmokwk/OPZ8vJpVP6w== <esenia1317@gmail.com>
2022-11-26 15:37:27 +0700

    added task38

---
+++ h5/task38/Program.cs
@@ -0,0 +1,37 @@
+﻿double[] FillArray()
+{
+    double[] ar = new double[10];
+    for (int i=0; i<10; i++)
+    {
+        ar[i] = new Random().Next(2, 100);
+    }
+    return ar;
+}
+
+double MaxMin(double[] coll)
+{
+    double max = coll[0];
+    double min = coll[0];
+    int l = coll.Length;
+    int count = 0;
+    double result = 0;
+    while(count != l)
+    {
+        if (coll[count] > max)
+        {
+            max = coll[count];
+        }
+        if (coll[count] < min)
+        {
+            min = coll[count];
+        }
+        count ++;
+    }
+    result = max - min;
+    return result;
+}
+
+double[] numbers = FillArray();
+Console.WriteLine("[" + string.Join(", ", numbers) + "]");
+
+Console.WriteLine($"Разница между максимальным и минимальным элементом массива = {MaxMin(numbers)}");
