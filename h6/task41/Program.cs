3a5d0fb0ea7cd034a0bfa73f7fbefdf41edd9db2
eJwtzE2PwiAUheH/wroLaCVt2fqRzGRiYsbJLAnQq0OLXIRWR43/XWK6OznJ8z6IMgZS2uMAngjSVhraqqYtVbXm2tS0YcArxjkwMIsDb0ooDWWkICHixXYQZ/qYS3KcU8c/lBu/6rE/19vzT7P6aod135f7z+Vmd/+4XwI1v9/M59SbyPEWIDsNKkLMbzL4PlR3sl6ESTtr5AC3IoLqBMZjHgGLKUEUcFLWFVeMw8HhNWP4DzZCkmokwk/OPZ8vJpVP6w== <esenia1317@gmail.com>
2022-11-30 19:35:17 +0700

    added task41

---
+++ h6/task41/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 
+// 0, 7, 8, -2, -2 -> 2
+// 
+// 1, -7, 567, 89, 223-> 3
+
+int Pos(int[] a)
+{
+    int count = 0;
+    for (int i=0; i<a.Length; i++)
+    {
+        if (a[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.Write("Введите длину массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[size];
+
+for (int i=0; i<size; i++)
+{
+    Console.Write($"Введите {i+1}-й элемент массива: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine($"{Pos(array)} число(ла, ел) больше 0");
