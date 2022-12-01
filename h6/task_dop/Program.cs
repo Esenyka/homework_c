c31a0dde32294f5e43d74c87ca504ec0440d5697
eJwtzE2PwiAUheH/wroLaCVt2fqRzGRiYsbJLAnQq0OLXIRWR43/XWK6OznJ8z6IMgZS2uMAngjSVhraqqYtVbXm2tS0YcArxjkwMIsDb0ooDWWkICHixXYQZ/qYS3KcU8c/lBu/6rE/19vzT7P6aod135f7z+Vmd/+4XwI1v9/M59SbyPEWIDsNKkLMbzL4PlR3sl6ESTtr5AC3IoLqBMZjHgGLKUEUcFLWFVeMw8HhNWP4DzZCkmokwk/OPZ8vJpVP6w== <esenia1317@gmail.com>
2022-12-01 14:16:42 +0700

    added taskdop

---
+++ h6/task_dop/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу для подсчета количества цифр в числе с помощью рекурсии.
+// 12345 -> 5
+
+int NumberOfDight(int n, int i)
+{
+    if (n <= 0)
+    {
+        return i;
+    }
+    else 
+    {
+        return NumberOfDight(n / 10, i + 1);
+    }
+}
+
+
+Console.WriteLine("Введите цифры: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(NumberOfDight(num, 0));
