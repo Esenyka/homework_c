6fad989bb2504d3e00aedcb7f2b4bc5dd85aeaa5
eJwtzE2PwiAUheH/wroLaCVt2fqRzGRiYsbJLAnQq0OLXIRWR43/XWK6OznJ8z6IMgZS2uMAngjSVhraqqYtVbXm2tS0YcArxjkwMIsDb0ooDWWkICHixXYQZ/qYS3KcU8c/lBu/6rE/19vzT7P6aod135f7z+Vmd/+4XwI1v9/M59SbyPEWIDsNKkLMbzL4PlR3sl6ESTtr5AC3IoLqBMZjHgGLKUEUcFLWFVeMw8HhNWP4DzZCkmokwk/OPZ8vJpVP6w== <esenia1317@gmail.com>
2022-12-04 21:15:06 +0700

    added task47

---
+++ h7/task47/Program.cs
@@ -0,0 +1,26 @@
+﻿double number = new Random().NextDouble();
+
+ Console.WriteLine(Math.Round(number, 2));
+
+Console.Write("Введите колличество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите колличество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] table = new double[m, n];
+
+void FillAndPrint(double[,] t)
+ {
+    for(int rows=0; rows<table.GetLength(0); rows++)
+    {
+        for (int colomns=0; colomns<table.GetLength(1); colomns++)
+        {
+            t[rows, colomns] = Math.Round((new Random().NextDouble()), 2);
+            Console.Write($" {t[rows, colomns]}");
+        }
+    Console.WriteLine(" "); 
+    }
+ }
+
+ FillAndPrint(table);
