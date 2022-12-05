8152d284d73d61d11418e59ef13a094400547b3c
eJwtzE2PwiAUheH/wroLaCVt2fqRzGRiYsbJLAnQq0OLXIRWR43/XWK6OznJ8z6IMgZS2uMAngjSVhraqqYtVbXm2tS0YcArxjkwMIsDb0ooDWWkICHixXYQZ/qYS3KcU8c/lBu/6rE/19vzT7P6aod135f7z+Vmd/+4XwI1v9/M59SbyPEWIDsNKkLMbzL4PlR3sl6ESTtr5AC3IoLqBMZjHgGLKUEUcFLWFVeMw8HhNWP4DzZCkmokwk/OPZ8vJpVP6w== <esenia1317@gmail.com>
2022-12-05 12:05:00 +0700

    added task52

---
+++ h7/task52/Program.cs
@@ -0,0 +1,30 @@
+﻿void Fill(int[,] t)
+{
+   for(int rows=0; rows<t.GetLength(0); rows++)
+   {
+        for (int colomns=0; colomns<t.GetLength(1); colomns++)
+        {
+            t[rows, colomns] = new Random().Next(0, 9);
+            Console.Write($" {t[rows, colomns]}");
+        }
+    Console.WriteLine(" "); 
+    }
+}
+
+double Something(int[,] t, int colomn)
+{
+    double result = 0;
+    for (int rows=0; rows<t.GetLength(0); rows++)
+    {
+        result = result + t[rows, colomn];    
+    }
+    return Math.Round(result/t.GetLength(0), 2);
+}
+
+int[,] table = new int[4, 4];
+Fill(table);
+
+for (int colomns=0; colomns<table.GetLength(1);colomns++)
+{
+    Console.Write($"{Something(table, colomns)}; ");
+}
